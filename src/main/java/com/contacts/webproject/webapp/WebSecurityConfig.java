04f819c6cabd2e1b47bad66caff2583bfed38a11
Joao Oliveira <Joao.Oliveira7@br.ey.com>
2023-04-17 16:19:51 -0300

    Implementando Spring-Security

--- src/main/java/com/contacts/webproject/webapp/WebSecurityConfig.java
+++ src/main/java/com/contacts/webproject/webapp/WebSecurityConfig.java
@@ -2,11 +2,13 @@ package com.contacts.webproject.webapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +21,22 @@ public class WebSecurityConfig {
 				.password("{bcrypt}$2a$13$qDELBBv8OdAwGwB7XFVYKOIL1spmPN7hGRQrZ5w5d9psFrUlFvcDa")
 				.roles("USER")
 				.build();
-		
+
 		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests((authorize) -> authorize
+						.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.permitAll())
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.invalidateHttpSession(true)
+						.deleteCookies());
+		return http.build();
 	}
 }
